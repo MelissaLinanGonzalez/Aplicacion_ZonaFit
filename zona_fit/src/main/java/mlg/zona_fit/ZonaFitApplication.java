76a9a865945b9803b3157cf8b62e39736403eae2
Melissa Liñán <melissa16800@gmail.com>
2025-06-10 19:00:07 +0200

    Configuracion del application.properties

--- zona_fit/src/main/java/mlg/zona_fit/ZonaFitApplication.java
+++ zona_fit/src/main/java/mlg/zona_fit/ZonaFitApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZonaFitApplication {
-// Comentario de prueba
+
 	public static void main(String[] args) {
 		SpringApplication.run(ZonaFitApplication.class, args);
 	}

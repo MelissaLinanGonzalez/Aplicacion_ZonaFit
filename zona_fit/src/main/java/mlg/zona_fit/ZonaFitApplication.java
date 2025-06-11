d440169071365b6858867acb5f7ef070a32c4963
Melissa Liñán <melissa16800@gmail.com>
2025-06-11 16:58:40 +0200

    Preparación para la creación del menú de la aplicación

--- zona_fit/src/main/java/mlg/zona_fit/ZonaFitApplication.java
+++ zona_fit/src/main/java/mlg/zona_fit/ZonaFitApplication.java
@@ -1,13 +1,30 @@
 package mlg.zona_fit;
 
+import mlg.zona_fit.service.IClienteServicio;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZonaFitApplication {
+public class ZonaFitApplication implements CommandLineRunner {
+
+	@Autowired
+	private IClienteServicio clienteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Iniciando la aplicación");
+		// Levanta la fábrica de spring
 		SpringApplication.run(ZonaFitApplication.class, args);
+		logger.info("Aplicación finalizada");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("*** Aplicación Zona Fit (GYM) ***");
+	}
 }

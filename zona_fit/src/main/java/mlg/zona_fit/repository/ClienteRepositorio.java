5861d4d9b55bef59f24a44d8e0566987488400b5
Melissa Liñán <melissa16800@gmail.com>
2025-06-11 16:32:05 +0200

    Implementación del paquete de servicio

--- zona_fit/src/main/java/mlg/zona_fit/repository/ClienteRepositorio.java
+++ zona_fit/src/main/java/mlg/zona_fit/repository/ClienteRepositorio.java
@@ -4,5 +4,5 @@ import mlg.zona_fit.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
-    
+
 }
---
+++ zona_fit/src/main/java/mlg/zona_fit/service/IClienteServicio.java
@@ -0,0 +1,12 @@
+package mlg.zona_fit.service;
+
+import mlg.zona_fit.model.Cliente;
+
+import java.util.List;
+
+public interface IClienteServicio {
+    public List<Cliente> listarClientes();
+    public Cliente buscarClientePorId(Cliente cliente);
+    public void guardarCliente(Cliente cliente); //Si el valor de la pk es nulo se hace una insercción, si tiene otro valor, realiza una actualizacion
+    public void eliminarCliente(Cliente cliente);
+}

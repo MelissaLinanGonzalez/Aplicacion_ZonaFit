c57464dbb39cda328ff76d92478971924dd06c58
Melissa Liñán <melissa16800@gmail.com>
2025-06-23 20:08:49 +0200

    Iniciación del Form de la Aplicación de zonaFit con Swing

---
+++ zona_fit/src/main/java/mlg/zona_fit/gui/ZonaFitForma.java
@@ -0,0 +1,20 @@
+package mlg.zona_fit.gui;
+
+import mlg.zona_fit.service.ClienteServicio;
+import mlg.zona_fit.service.IClienteServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+@Component
+public class ZonaFitForma extends JFrame{
+    private JPanel panelPrincipal;
+    IClienteServicio clienteServicio;
+
+    @Autowired
+    public ZonaFitForma(ClienteServicio clienteServicio){
+        this.clienteServicio = clienteServicio;
+
+    }
+}

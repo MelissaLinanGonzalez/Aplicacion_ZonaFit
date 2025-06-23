646bf3e62d83214ec0159f1ab15eb549e3d9c811
Melissa Liñán <melissa16800@gmail.com>
2025-06-23 20:25:34 +0200

    Arranque de la aplicación con Spring desde el formulario

---
+++ zona_fit/src/main/java/mlg/zona_fit/ZonaFitSwing.java
@@ -0,0 +1,23 @@
+package mlg.zona_fit;
+
+import mlg.zona_fit.gui.ZonaFitForma;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ZonaFitSwing {
+    public static void main(String[] args) {
+        // INstanciar la fábrica de Spring
+        ConfigurableApplicationContext contextoSpring = new SpringApplicationBuilder(ZonaFitSwing.class).headless(false).web(WebApplicationType.NONE).run(args);
+
+        // Crear un objeto de Swing a partir de la fábrica de Spring
+        SwingUtilities.invokeLater(() -> {
+            ZonaFitForma zonaFitForma = contextoSpring.getBean(ZonaFitForma.class);
+            zonaFitForma.setVisible(true);
+        });
+    }
+}
